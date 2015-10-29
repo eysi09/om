6cf791410ee85397c86fb78f6976b6db916d0301
Eyþór Magnússon <eysi09@gmail.com>
2015-10-29 23:12:45 +0100

    Init commit.

---
+++ om.js
@@ -0,0 +1,28 @@
+
+$(function() {
+	$("#album1").click(function() {
+		$(".full-overlay-14").fadeIn(600);
+		});
+	});
+		
+		
+$(function() {
+	$(".full-overlay-14").click(function() {
+		$(".full-overlay-14").fadeOut(600);
+		});
+	});
+	
+$(function() {
+	$("#album2").click(function() {
+		$(".full-overlay-24").fadeIn(600);
+		});
+	});
+		
+		
+$(function() {
+	$(".full-overlay-24").click(function() {
+		$(".full-overlay-24").fadeOut(600);
+		});
+	});
+
+
